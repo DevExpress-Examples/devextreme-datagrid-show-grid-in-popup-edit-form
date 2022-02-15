a7f23a9061168d827e5d9935921e02c9fdaa24e5
Mark Allen Ramirez <markallenramirez@gmail.com>
2022-02-15 11:51:53 +0800

    Add Vue

---
+++ Vue/src/components/SubjectsEditorComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <DxDataGrid
+    :data-source="dataSource"
+    key-expr="SubjectCode"
+    :show-borders="true"
+    :on-saved="onSaved"
+    :on-initialized="onInitialized"
+  >
+    <DxColumn data-field="SubjectCode">
+      <DxRequiredRule/>  
+    </DxColumn>>
+    <DxColumn data-field="SubjectName">
+      <DxRequiredRule/>
+    </DxColumn>>
+    <DxColumn data-field="Section">
+      <DxRequiredRule/>
+    </DxColumn>>
+
+    <DxEditing
+      mode="row"
+      :allow-adding="true"
+      :allow-updating="true"
+      :allow-deleting="true"
+    />
+  </DxDataGrid>
+</template>
+<script>
+import { DxDataGrid, DxColumn, DxEditing, DxRequiredRule } from "devextreme-vue/data-grid";
+
+export default {
+  components: {
+    DxDataGrid,
+    DxColumn,
+    DxEditing,
+    DxRequiredRule
+  },
+  props: {
+    dataSource: {
+      type: Object,
+      default: () => {},
+    },
+    onSaved: {
+      type: Function,
+      default: () => function () {},
+    },
+    onInitialized: {
+      type: Function,
+      default: () => function () {},
+    }
+  },
+};
+</script>
