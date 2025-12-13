ec4345351f860caeb91b8ac47d9738e3d9299510
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-12-13 10:54:28 +0200

    Migrate to eslint-config-devextreme 1.1.8

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,110 +1,20 @@
-import { Component, ViewChild } from '@angular/core';
-import { DxDataGridComponent, DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
-import DataSource from 'devextreme/data/data_source';
-import {
-  Service, Student, Subject, StudentSubject,
-} from './app.service';
+import { Component } from '@angular/core';
+import { ClickEvent } from 'devextreme/ui/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [Service],
 })
 export class AppComponent {
-  @ViewChild('mainGrid', { static: false }) mainGrid!: DxDataGridComponent;
+  title = 'Angular';
 
-  title = 'Student Subjects Management';
+  counter = 0;
 
-  dataSource: DataSource;
+  buttonText = 'Click count: 0';
 
-  students: Student[] = [];
-
-  subjects: Subject[] = [];
-
-  canBeSaved = false;
-
-  popupInstance: any;
-
-  key: string | number | null = null;
-
-  constructor(private readonly service: Service) {
-    this.students = service.getStudents();
-    this.dataSource = new DataSource({
-      store: {
-        type: 'array',
-        key: 'ID',
-        data: service.getStudentSubjects(),
-      },
-    });
+  onClick(_e: ClickEvent): void {
+    this.counter++;
+    this.buttonText = `Click count: ${this.counter}`;
   }
-
-  onInitNewRow(e: DxDataGridTypes.InitNewRowEvent): void {
-    this.subjects = [];
-  }
-
-  onEditingStart(e: DxDataGridTypes.EditingStartEvent): void {
-    this.subjects = [...e.data.Subjects ?? []];
-  }
-
-  onEditorPreparing(e: DxDataGridTypes.EditorPreparingEvent): void {
-    this.canBeSaved = e.row?.isNewRow ?? false;
-    this.key = e.row?.key;
-  }
-
-  onPopupContentReady(e: any): void {
-    this.popupInstance = e.component;
-    if (this.canBeSaved) {
-      e.component.option('toolbarItems[0].disabled', true);
-      this.canBeSaved = false;
-    }
-  }
-
-  onSubjectEditingStart(e: DxDataGridTypes.EditingStartEvent): void {
-    if (this.popupInstance) {
-      this.popupInstance.option('toolbarItems[0].disabled', true);
-    }
-  }
-
-  onSubjectRowValidating(e: DxDataGridTypes.RowValidatingEvent): void {
-    if (this.popupInstance) {
-      if (e.isValid) {
-        this.popupInstance.option('toolbarItems[0].disabled', false);
-      } else {
-        this.popupInstance.option('toolbarItems[0].disabled', true);
-      }
-    }
-  }
-
-  onSubjectSaved(e: DxDataGridTypes.SavedEvent): void {
-    this.subjects = e.component.getDataSource().items();
-    if (this.popupInstance) {
-      if (this.subjects.length > 0) {
-        this.popupInstance.option('toolbarItems[0].disabled', false);
-      } else {
-        this.popupInstance.option('toolbarItems[0].disabled', true);
-      }
-    }
-  }
-
-  onSaving(e: DxDataGridTypes.SavingEvent): void {
-    if (e.changes[0]) {
-      if (e.changes[0].data) {
-        e.changes[0].data.Subjects = this.subjects;
-      }
-      return;
-    }
-    e.changes.push({
-      data: { Subjects: this.subjects },
-      key: this.key,
-      type: 'update',
-    });
-  }
-
-  customizeText = (cellInfo: { value?: Subject[]; target?: string }): string => {
-    if (cellInfo.value && cellInfo.value.length > 0) {
-      return cellInfo.value.reduce((text: string, subject: Subject) => `${text}${subject.Name}, `, '').slice(0, -2);
-    }
-    return '';
-  };
 }
