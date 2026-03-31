9d3f62e27714963d706b86114f614b0d66db80d3
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-03-31 10:23:59 +0300

    Fix issues after migration

--- ASP.NET Core/Models/StudentSubject.cs
+++ ASP.NET Core/Models/StudentSubject.cs
@@ -7,10 +7,10 @@ namespace ASP.NET_Core.Models
     public class StudentSubject
     {
         public int ID { get; set; }
-        
+
         [Required]
         public int StudentID { get; set; }
-        
+
         public List<Subject> Subjects { get; set; } = new List<Subject>();
     }
 }
