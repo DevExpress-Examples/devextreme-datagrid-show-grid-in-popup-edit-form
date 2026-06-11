6f30fd6dd2eb4079368f41ef880550553711cec3
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-11 21:33:09 +0300

    Fix DevExtreme licensing import order

--- Vue/src/main.ts
+++ Vue/src/main.ts
@@ -1,12 +1,10 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'devextreme/dist/css/dx.material.blue.light.compact.css';
 import config from 'devextreme/core/config';
 import { licenseKey } from './devextreme-license';
 
 config({ licenseKey });
-
-import { createApp } from 'vue';
-import App from './App.vue';
-
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
 
 const app = createApp(App);
 
